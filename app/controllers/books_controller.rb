c56c91bc0d2b41bf2f601447669a12f90d6b82ae
Sydney_Hunter <sydney.jane.hunter@gmail.com>
2020-03-31 17:25:43 -0600

    added books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,45 @@
 class BooksController < ApplicationController 
-
-end
+    get "/books" do
+      redirect_if_not_logged_in 
+      @books = Book.all
+      erb :'books/index'
+    end
+  
+    get "/books/new" do
+      redirect_if_not_logged_in 
+      @error_message = params[:error]
+      erb :'books/new'
+    end
+  
+    get "/books/:id/edit" do
+      redirect_if_not_logged_in 
+      @error_message = params[:error]
+      @book = Book.find(params[:id])
+      erb :'books/edit'
+    end
+  
+    post "/books/:id" do
+      redirect_if_not_logged_in 
+      @book = Book.find(params[:id])
+      unless Book.valid_params?(params)
+        redirect "/books/#{@book.id}/edit?error=invalid book"
+      end
+      @book.update(params.select{|k|k=="name" || k=="author" || k=="bookshelf_id"})
+      redirect "/books/#{@book.id}"
+    end
+  
+    get "/books/:id" do
+      redirect_if_not_logged_in 
+      @book = Book.find(params[:id])
+      erb :'books/show'
+    end
+  
+    post "/books" do
+      redirect_if_not_logged_in 
+      unless Book.valid_params?(params)
+        redirect "/books/new?error=invalid book"
+      end
+      Book.create(params)
+      redirect "/books"
+    end
+  end
